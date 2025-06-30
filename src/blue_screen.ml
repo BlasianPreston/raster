993122100ed726caa5bb3bb4ad2c5a7ae182d4ac
Preston Williams <prestonwilliams987@gmail.com>
2025-06-30 18:13:50 +0000

    finished blue screen

--- src/blue_screen.ml
+++ src/blue_screen.ml
@@ -5,11 +5,17 @@ open Core
    the corresponding position in the background image instead of
    just ignoring the background image and returning the foreground image.
 *)
-let transform ~foreground ~background:_ = foreground
+let transform ~foreground ~background =
+  Image.mapi foreground ~f:(fun ~x ~y pixel ->
+    if Pixel.blue pixel > Pixel.red pixel + Pixel.green pixel
+    then Image.get background ~x ~y
+    else pixel)
+;;
 
 let command =
   Command.basic
-    ~summary:"Replace the 'blue' pixels of an image with those from another image"
+    ~summary:
+      "Replace the 'blue' pixels of an image with those from another image"
     [%map_open.Command
       let foreground_file =
         flag
@@ -28,5 +34,32 @@ let command =
         let image' = transform ~foreground ~background in
         Image.save_ppm
           image'
-          ~filename:(String.chop_suffix_exn foreground_file ~suffix:".ppm" ^ "_vfx.ppm")]
+          ~filename:
+            (String.chop_suffix_exn foreground_file ~suffix:".ppm"
+             ^ "_vfx.ppm")]
 ;;
+
+let test_helper x y image pixel =
+  let expected_result = Image.get image ~x ~y in
+  Pixel.equal expected_result pixel
+;;
+
+let%expect_test "blue_screen" =
+  let filename = "../images/oz_bluescreen.ppm" in
+  let test_file_name = "../images/reference-oz_bluescreen_vfx.ppm" in
+  let background = Image.load_ppm ~filename:"../images/meadow.ppm" in
+  let image = Image.load_ppm ~filename in
+  let transformed_img = transform ~background ~foreground:image
+in
+let expected_image = Image.load_ppm ~filename:test_file_name in
+let result =
+  if
+    Image.width transformed_img = Image.width expected_image
+    && Image.height transformed_img = Image.height expected_image
+  then
+    Image.foldi transformed_img ~init:0 ~f:(fun ~x ~y acc pixel ->
+      if test_helper x y expected_image pixel then acc else acc + 1)
+  else -1
+in
+print_s [%message (result : int)];
+[%expect {| (result 0) |}]
