02f246f8189136c02f989854b703368ab18b2780
Preston Williams <prestonwilliams987@gmail.com>
2025-06-30 17:42:31 +0000

    finished first expect test

--- src/grayscale.ml
+++ src/grayscale.ml
@@ -3,7 +3,9 @@ open Core
 (* You need to change the implementation of this function so that it does something
    to the image instead of just leaving it untouched. *)
 let transform image =
-  Image.map image ~f:(fun (r, g, b) -> let gray = (r + g + b) / 3 in (gray, gray, gray))
+  Image.map image ~f:(fun (r, g, b) ->
+    let gray = (r + g + b) / 3 in
+    gray, gray, gray)
 ;;
 
 let command =
@@ -20,5 +22,29 @@ let command =
         let image = Image.load_ppm ~filename |> transform in
         Image.save_ppm
           image
-          ~filename:(String.chop_suffix_exn filename ~suffix:".ppm" ^ "_gray.ppm")]
+          ~filename:
+            (String.chop_suffix_exn filename ~suffix:".ppm" ^ "_gray.ppm")]
+;;
+
+let test_helper x y image pixel =
+  let expected_result = Image.get image ~x ~y in
+  Pixel.equal expected_result pixel
+;;
+
+let%expect_test ("grayscale" ) =
+  let filename = "../images/beach_portrait.ppm" in
+  let test_file_name = "../images/reference-beach_portrait_gray.ppm" in
+  let image = Image.load_ppm ~filename |> transform in
+  let expected_image = Image.load_ppm ~filename:test_file_name in
+  let result =
+    if
+      Image.width image = Image.width expected_image
+      && Image.height image = Image.height expected_image
+    then
+      Image.foldi image ~init:0 ~f:(fun ~x ~y acc pixel ->
+        if test_helper x y expected_image pixel then acc else acc + 1)
+    else -1
+  in
+  print_s [%message (result : int)];
+  [%expect {| (result 0) |}]
 ;;
